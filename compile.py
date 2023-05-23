777f47a88f5058ef5b48298aa67053ca8478d455
blockdev <1689531+0xbok@users.noreply.github.com>
2023-05-23 12:06:38 +0400

    customize

--- compile.py
+++ compile.py
@@ -15,12 +15,10 @@ import config
 REPO = config.REPO
 github = Github(config.TOKEN)
 
-SEVERITY_LABELS = ['Severity: Critical Risk', 'Severity: High Risk', 'Severity: Medium Risk', 'Severity: Low Risk', 'Severity: Gas Optimization', 'Severity: Informational']
+SEVERITY_LABELS = ['Critical', 'High', 'Medium', 'Low', 'Gas', 'Informational']
 
 issue_dict : dict[str, list[str]] = {}
 
-# TODO catch get_repo() 404 errors and produce a gentle suggestion on what's wrong.
-# "GitHub's REST API v3 considers every pull request an issue"--need to filter them out.
 for issue in github.get_repo(REPO).get_issues():
     if issue.state == 'open' and issue.pull_request is None:
         # filter issue labels for only severity labels
@@ -30,10 +28,18 @@ for issue in github.get_repo(REPO).get_issues():
         label = issue.labels[0].name
         if label not in issue_dict:
            issue_dict[label] = []
-        issue_dict[label].append(f"### {issue.title} \n{issue.body}\n")
+        issue_dict[label].append(f"### PEPE. {label} - {issue.title} \n{issue.body}\n")
 
 with open("report.md", "w") as report:
     for label in SEVERITY_LABELS:
-        report.write(f"## {label[10:]}\n\n")
-        for content in issue_dict[label]:
-            report.write(content.replace("\r\n", "\n"))
+        num = 1
+        title = f"{label} Findings"
+        if label == "Gas":
+            title = "Gas Saving Findings"
+        report.write(f"## {title}\n\n")
+        if label in issue_dict.keys():
+            for content in issue_dict[label]:
+                replaced = content.replace("\r\n", "\n")
+                replaced = replaced.replace("PEPE", f"{num}")
+                report.write(replaced)
+                num += 1
