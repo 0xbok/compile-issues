d59492e69fb275e968854acfa7d2af87c29a3194
engn33r <engn33r@users.noreply.github.com>
2024-01-06 00:00:00 +0000

    apply fix for reversed issue

--- compile.py
+++ compile.py
@@ -19,19 +19,25 @@ SEVERITY_LABELS = ['Critical', 'High', 'Medium', 'Low', 'Gas', 'Informational']
 
 issue_dict : dict[str, list[str]] = {}
 counters: dict[str, int] = {}
-issues = github.get_repo(REPO).get_issues(state='open')
-for issue in issues.reversed:
-    if issue.pull_request is None:
-        # filter issue labels for only severity labels
-        severity_labels_in_issue = [label.name for label in issue.labels if label.name in SEVERITY_LABELS]
+issues_list = github.get_repo(REPO).get_issues(state='open')
 
-        assert len(severity_labels_in_issue) == 1, f"Issue {issue.html_url} has {len(severity_labels_in_issue)} severity label."
-        label = issue.labels[0].name
-        if label not in issue_dict:
-           issue_dict[label] = []
-           counters[label] = 0
-        counters[label]+=1
-        issue_dict[label].append(f"### {counters[label]}. {label} - {issue.title}\n\n{issue.body}\n\n")
+# Apply fix from this issue https://github.com/PyGithub/PyGithub/issues/1136
+for i in range(issues_list.totalCount//30, -1, -1):
+    page = issues_list.get_page(i)
+    page.reverse()
+    for issue in page:
+        if issue.pull_request is None:
+        # filter issue labels for only severity labels
+            severity_labels_in_issue = [label.name for label in issue.labels if label.name in SEVERITY_LABELS]
+
+            assert len(severity_labels_in_issue) == 1, f"Issue {issue.html_url} has {len(severity_labels_in_issue)} severity label."
+            label = issue.labels[0].name
+            print(label)
+            if label not in issue_dict:
+                issue_dict[label] = []
+                counters[label] = 0
+            counters[label]+=1
+            issue_dict[label].append(f"### {counters[label]}. {label} - {issue.title}\n\n{issue.body}\n\n")
 
 with open("report.md", "w") as report:
     for label in SEVERITY_LABELS:
